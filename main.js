67ca9276dce109a31e170d449257756050932610
Sonalika2008 <84615828+Sonalika2008@users.noreply.github.com>
2022-09-21 19:34:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', modelReady);
+}
+
